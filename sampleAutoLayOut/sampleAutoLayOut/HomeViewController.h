deeff14542da1addb42fc609337ea3e6e459ef02
Emam Shaikh <emam.shaikh@gmail.com>
2018-01-06 18:15:27 +0530

    fixed issue

---
+++ sampleAutoLayOut/sampleAutoLayOut/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  sampleAutoLayOut
+//
+//  Created by My Admin on 03/01/18.
+//  Copyright © 2018 Emam Shaikh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
